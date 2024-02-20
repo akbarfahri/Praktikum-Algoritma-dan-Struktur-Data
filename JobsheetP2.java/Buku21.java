461037d8d5b11aef0b48611452fbfb61af7c9882
akbarfahri <akbardaffa007@gmail.com>
2024-02-20 10:40:11 +0700

    P2_Latihan Praktikum_21

--- JobsheetP2.java/Buku21.java
+++ JobsheetP2.java/Buku21.java
@@ -1,6 +1,17 @@
 class Buku21 {
 String judul, pengarang;
-int halaman, stok, harga;
+int halaman, stok, harga, terjual;
+public Buku21() {
+
+    }
+    public Buku21(String jud, String pg, int hal, int stok, int har) {
+        judul = jud;
+        pengarang = pg;
+        halaman = hal;
+        this.stok = stok;
+        harga = har;
+
+    }
     void tampilinformasi() {
         System.out.println("Judul: " + judul);
         System.out.println("Pengarang: " + pengarang);
@@ -12,6 +23,7 @@ int halaman, stok, harga;
     void terjual (int jml) {
         if (stok>0) {
             stok -= jml;
+            terjual= jml;
         }
     }
     void restock(int jml) {
@@ -20,15 +32,27 @@ int halaman, stok, harga;
     void  gantiHarga(int hrg) {
         harga = hrg;
     }
-    public Buku21() {
-
+    int hitunghargaTotal(){
+        int hargaTot = terjual * harga;
+        return hargaTot;
     }
-    public Buku21(String jud, String pg, int hal, int stok, int har) {
-        judul = jud;
-        pengarang = pg;
-        halaman = hal;
-        this.stok = stok;
-        harga = har;
-
+    int hitungDiskon(){
+        if (hitunghargaTotal()> 150000) {
+        double diskon = hitunghargaTotal() * 0.12;
+        int intDiskon = (int)diskon;
+        return intDiskon;
+    } else if ((hitunghargaTotal() > 75000) && (hitunghargaTotal() <= 150000)){
+        double diskon = hitunghargaTotal() * 0.05;
+        int intDiskon = (int)diskon;
+        return intDiskon;
+    } else {
+        double diskon = hitunghargaTotal() * 0;
+        int intDiskon = (int)diskon;
+        return intDiskon;
+    }
+    }
+    int hitunghargabayar(){
+        int hargatotal = hitunghargaTotal() - hitungDiskon();
+        return hargatotal;
     }
 }

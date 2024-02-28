1936f2739e43ea7d2d1aa581f2e7fe498d056469
akbarfahri <akbardaffa007@gmail.com>
2024-02-28 21:50:05 +0700

    Praktikum3_balok_jobsheet3_21

---
+++ JobsheetP2.java/ArrayObjects/balok.java
@@ -0,0 +1,20 @@
+package ArrayObjects;
+
+public class balok {
+    public int panjang;
+    public int lebar;
+    public int tinggi;
+    public balok (int p,int l, int t) {
+
+    
+        panjang = p;
+        lebar = l;
+        tinggi = t;
+    }
+
+    public int hitungVolume () {
+        return panjang * lebar * tinggi;
+    }
+
+    
+}

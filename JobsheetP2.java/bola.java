d83d0d984ac7c018ed5db05c9692400aa3098157
akbarfahri <akbardaffa007@gmail.com>
2024-03-03 17:06:06 +0700

    bola.class

---
+++ JobsheetP2.java/bola.java
@@ -0,0 +1,17 @@
+public class bola {
+    
+    double jariJari;
+
+    public bola(double jariJari) {
+        this.jariJari = jariJari;
+    }
+
+    public double hitungLuasPermukaan() {
+        return 4 * Math.PI * jariJari * jariJari;
+    }
+
+    public double hitungVolume() {
+        return (4.0 / 3.0) * Math.PI * jariJari * jariJari * jariJari;
+    }
+
+}

e9ec5b1d255c1db77782aa701d75f990ea5ca2a8
akbarfahri <akbardaffa007@gmail.com>
2024-05-14 09:48:33 +0700

    Praktikum10pertemuan11_M Akbar F D_21_1G

---
+++ ALGORITMA DAN STRUKTUR DATA/ALGORITMA DAN STRUKTUR DATA/src/Praktikum11/Node.java
@@ -0,0 +1,13 @@
+package Praktikum11;
+
+public class Node { 
+    int data;
+    Node next;
+    Node (int nilai, Node berikutnya){
+        data=nilai;
+        next=berikutnya;
+    }
+    Node (){
+        
+    }
+}

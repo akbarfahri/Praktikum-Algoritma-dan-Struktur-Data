0100317c14dfcccbbc83c0ee9b124135b678fb46
akbarfahri <akbardaffa007@gmail.com>
2024-05-30 23:08:26 +0700

    percobaan 2 asd Tree jobsheet11

---
+++ ALGORITMA DAN STRUKTUR DATA/ALGORITMA DAN STRUKTUR DATA/src/Tree/BinaryTreeArrayMain21.java
@@ -0,0 +1,15 @@
+package Tree;
+
+public class BinaryTreeArrayMain21 {
+    public static void main(String[] args) {
+        BinaryTreeArray21 bta = new BinaryTreeArray21();
+        int[] data = { 6, 4, 8, 3, 5, 7, 9, 0, 0, 0 };
+        int idxLast = 6;
+        bta.populateData(data, idxLast);
+
+        System.out.print("\n InOrder Traversal : ");
+        bta.traverseInOrder(0);
+        System.out.println("\n");
+    }
+}
+
